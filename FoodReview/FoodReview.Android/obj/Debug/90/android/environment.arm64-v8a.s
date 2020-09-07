99fad4625e3444f53d9983bdd1a0f9533457182b
MadCoderBubt <sudhanandabiswas@yahoo.com>
2020-09-07 13:56:45 +0600

    register page

--- FoodReview/FoodReview.Android/obj/Debug/90/android/environment.arm64-v8a.s
+++ FoodReview/FoodReview.Android/obj/Debug/90/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"a515d3f9-3786-47f9-8422-931c2bca71be"
+	.asciz	"337cff23-f3ff-4c43-869a-98a373991c21"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
